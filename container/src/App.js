93fda47da8b66e1165cce94431f55c55c0534836
Rupak Chatterjee <rupakchttrj09@gmail.com>
2024-04-14 20:28:56 +0530

    added css classname prefix for prod

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "con", //to remove css-in-js classname conflicts if subprojects also use the same css-in-js library
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
