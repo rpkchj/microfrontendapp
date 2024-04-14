18e0c1d3045e3730155e69b57f1b7b18e1263cb1
Rupak Chatterjee <rupakchttrj09@gmail.com>
2024-04-14 21:53:27 +0530

    Container to Marketing and ViceVersa routing communication implemented

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,22 @@
 import {mount as mountMarketing} from 'marketing/MarketingApp'
 import React,{useRef, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
 
 export default() => {
     const ref = useRef(null) //reference to an HTML element for rendering
+    const history = useHistory() //Browser History object
 
     useEffect(() => {
-        mountMarketing(ref.current)
+        const {onParentNavigate} = mountMarketing(ref.current, {
+            onNavigate: ({pathname: nextPathName}) => {
+                const {pathname: currentPath} = history.location
+                if(currentPath !== nextPathName){ //to check if Container is already at the desired location and prevent infinite callback calls
+                    history.push(nextPathName)
+                }
+            }
+        })
+
+        history.listen(onParentNavigate) //listening for navigation changes in Container and calling the sub-app callback function
     },[])
 
     return <div ref={ref}/>
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createMemoryHistory, createBrowserHistory} from 'history'
 import App from './App'
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory() //Memory History object
+
+    if(onNavigate){
+    history.listen(onNavigate) // callback from Container to sync any routing changes in Marketing sub-app
+    }
+
     ReactDOM.render(
-        <App/>,
+        <App history={history}/>,
         el
     )
+
+    return {
+        onParentNavigate({pathname: nextPathName}){
+            const {pathname: currentPath} = history.location
+
+            if(currentPath !== nextPathName){
+                history.push(nextPathName)
+            }
+        }
+    }
 }
 
 if(process.env.NODE_ENV === "development"){
     const devRoot = document.querySelector('#_marketing-dev-root')
 
     if(devRoot){
-        mount(devRoot)
+        mount(devRoot, {defaultHistory: createBrowserHistory()})
     }
 }
 
